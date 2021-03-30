47e53056112cd731f52ffc519ab9c4d7d8c15755
Anthony Guamán <aguaman@zenttinel.com.ec>
2021-03-30 12:33:44 -0500

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AsociadoComponent } from './components/asociado/asociado.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { ConductorComponent } from './components/conductor/conductor.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent},
+  { path: 'cliente', component: ClienteComponent},
+  { path: 'asociado', component: AsociadoComponent},
+  { path: 'conductor', component: ConductorComponent},
+ 
+
+
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsociadoComponent,
+    InicioComponent,
+    ClienteComponent,
+    PrincipalComponent,
+    ConductorComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)]
   ],
   providers: [],
   bootstrap: [AppComponent]
